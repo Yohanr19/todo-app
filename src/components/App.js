1a832fdde9da780c71761d2f41b07b5796b1cdea
Yohanr19 <yohanreyes@outlook.com>
2021-07-04 19:31:14 -0400

    changed fetch behavior, now each request returns relevant data, cutting the requests in half

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Options from './Options';
 import {v1 as uuidv1 } from 'uuid'
 
 function App() {
+  const serverUrl = 'http://127.0.0.1:3001'
   const [isDark, setIsDark] = useState(true);
   const [data, setData] = useState('loading');
   const [filter, setFilter] = useState('all')
@@ -18,26 +19,41 @@ function App() {
       setFilter(string)
   }
   const deleteHandler = async (id)=> {
-    await fetch('http://127.0.0.1:3001/api/todo',{method:'DELETE', body: JSON.stringify(id)}).catch(error=> console.log(error))
-    getData()
+    await fetch(`${serverUrl}/api/todo`,{method:'DELETE', body: JSON.stringify(id)}).then((response) =>{
+      response.json().then(json =>{ 
+          setData(json);
+        })
+      }).catch(error=> console.log(error))
   }
   const clearCompleted = async () => {
-    await fetch('http://127.0.0.1:3001/api/todo',{method:'DELETE', body: JSON.stringify('completed')}).catch(error=> console.log(error))
-    getData()
+    await fetch(`${serverUrl}/api/todo`,{method:'DELETE', body: JSON.stringify('completed')}).then((response) =>{
+      response.json().then(json =>{ 
+          setData(json);
+        })
+      }).catch(error=> console.log(error))
   }
   const isActiveHandler = async (id)=> {
-    await fetch('http://127.0.0.1:3001/api/todo',{method:'PUT', body: JSON.stringify(id)}).catch(error=> console.log(error))
-    getData()
+    await fetch(`${serverUrl}/api/todo`,{method:'PUT', body: JSON.stringify(id)}).then((response) =>{
+      response.json().then(json =>{ 
+          setData(json);
+        })
+      }).catch(error=> console.log(error))
  }
  const getData= async ()=> {
-    await fetch('http://localhost:3001/api/todo', {method: 'GET'}).then(response => response.json()).then(json =>{ 
-        setData(json);
+    await fetch(`${serverUrl}/api/todo`, {method: 'GET'}).then((response) =>{
+      response.json().then(json =>{ 
+          setData(json);
+        })
       }).catch(error => {
         console.log(error)
       }) 
  }
  const postItem = async (body)=>{
-    await fetch('http://localhost:3001/api/todo', {method: 'POST', body: JSON.stringify(body)}).catch(error => console.log(error))
+    await fetch(`${serverUrl}/api/todo`, {method: 'POST', body: JSON.stringify(body)}).then((response) =>{
+      response.json().then(json =>{ 
+          setData(json);
+        })
+      }).catch(error => console.log(error))
  }
 
  const createItem = (value)=> {
@@ -47,7 +63,6 @@ function App() {
           isActive: true,
        }
       postItem(newItem);
-      getData()
  }
   return (
     <div className={"app "+(isDark?"dark-bg":"light-bg")}>
